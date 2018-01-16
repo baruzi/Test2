00c2bb0c580b52491497531463725028a344124d
baruzi <baruzi33@hotmail.com>
2018-01-16 14:26:51 +0900

    Source commit

---
+++ HelloCpp/HelloCpp.cpp
@@ -0,0 +1,16 @@
+// HelloCpp.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello 1";
+	cout << "hello 2";
+	cout << "hello 3";
+	cout << "hello 4";
+    return 0;
+}
+
